82adfdcc3b9389c75c766b49dd6477b816a32511
Divakar <dvkr22@gmail.com>
2023-02-28 12:28:26 +0530

    added

--- Sensu/init.sh
+++ Sensu/init.sh
@@ -28,15 +28,6 @@ sensu
 192.168.1.55 salt
 EOF
 echo "updated all files"
-sudo yum install git -y
-cd $HOME
-mkdir git
-cd git
-git clone https://github.com/DivakarK22/scripts.git
-cd scripts/setups/
-sudo bash installsaltMinion.sh
-echo "Installed Salt-minion"
-sudo systemctl restart salt-minion
 cat <<EOF > /etc/ssh/sshd_config
 HostKey /etc/ssh/ssh_host_rsa_key
 HostKey /etc/ssh/ssh_host_ecdsa_key
@@ -55,19 +46,4 @@ AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE
 AcceptEnv XMODIFIERS
 Subsystem       sftp    /usr/libexec/openssh/sftp-server
 EOF
-sudo yum install wget -y
-sudo yum -y install epel-release
-cd ~ && wget https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_23.3.1-1~centos~7_amd64.rpm
-sudo yum -y install esl-erlang*.rpm
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.19/rabbitmq-server-3.8.19-1.el7.noarch.rpm
-sudo yum -y install rabbitmq-server*.rpm
-sudo systemctl start rabbitmq-server.service
-sudo systemctl enable rabbitmq-server.service
-sudo rabbitmq-plugins enable rabbitmq_management
-sudo yum install redis -y
-echo "all good"
-cd $HOME
-cd git/scripts/setups
-sudo bash ruby.sh
-sudo yum install sensu uchiwa -y
 sudo reboot
